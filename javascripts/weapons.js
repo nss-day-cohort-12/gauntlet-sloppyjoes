ca8d7cbfcb656203e8f6965e82fb4a2f492fcad6
Steven Tyler Roberts <steventylerroberts@Stevens-MacBook-Pro.local>
2016-03-02 11:31:55 -0600

    Added basic battlefield page and population functions

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -9,21 +9,21 @@ var Weapon = function() {
 };
 
 var Dagger = function() {
-  this.name = "dagger";
+  this.name = "Dagger";
   this.damage = 4;
   this.hands = 1;
 };
 Dagger.prototype = new Weapon();
 
 var BroadSword = function() {
-  this.name = "broad sword";
+  this.name = "Broad Sword";
   this.damage = 14;
   this.hands = 2;
 };
 BroadSword.prototype = new Weapon();
 
 var WarAxe = function() {
-  this.name = "war axe";
+  this.name = "War Axe";
   this.damage = 18;
   this.hands = 2;
 };
