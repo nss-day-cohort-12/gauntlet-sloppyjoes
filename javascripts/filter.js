60f8b2be1713a274f95b577ac30b8fb6344b4206
Steven Tyler Roberts <steventylerroberts@Stevens-MacBook-Pro.local>
2016-03-02 21:56:45 -0600

    Added combat function, weapon and spell filtering and writing, and reset button functionality

--- javascripts/filter.js
+++ javascripts/filter.js
@@ -1,3 +1,11 @@
 function writeClass(string){
 	$(".middle-column").append(`<div class="card__button classButton"><a class="class__link btn btn--big btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text character-class">${string}</span></a></div>`);
 };
+
+function writeWeapon(string){
+	$(".weapon-column").append(`<div class="card__button weaponButton"><a class="class__link btn btn--big btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text character-weapon">${string}</span></a></div>`);
+};
+
+function writeSpell(string){
+	$(".spell-column").append(`<div class="card__button spellButton"><a class="class__link btn btn--big btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text character-spell">${string}</span></a></div>`);
+};
